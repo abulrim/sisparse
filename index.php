82510703dd96e5291bd624174f0d033309d65786
Rami Rizk <ramirizk90@gmail.com>
2013-04-08 10:24:33 +0300

    Bug fixes

--- index.php
+++ index.php
@@ -1,137 +1,148 @@
 <?php
 
-    //generic function
+//generic function
 
-    function cleanField($field) {
-        //decode any html character
-        $field = html_entity_decode($field);
+function cleanField($field) {
+	//decode any html character
+	$field = html_entity_decode($field);
 
-        //replace white space char to ' '
-        //http://stackoverflow.com/questions/6275380/does-html-entity-decode-replaces-nbsp-also-if-not-how-to-replace-it
-        $field = str_replace("\xA0", ' ', $field);
+	//replace white space char to ' '
+	//http://stackoverflow.com/questions/6275380/does-html-entity-decode-replaces-nbsp-also-if-not-how-to-replace-it
+	$field = str_replace("\xA0", ' ', $field);
 
-        //remove html characters
-        $field = strip_tags($field);
+	//remove html characters
+	$field = strip_tags($field);
 
-        //escape field
-        $field = mysql_real_escape_string($field);
-        
-        //remove whitespace
-        $field = trim($field);
-        return $field;
-    }
+	//escape field
+	$field = mysql_real_escape_string($field);
 
-    function parseTime($time) {
-        if (empty($time)) {
-            return array(NULL, NULL);
-        }
-        $parsedTime = array();
-        $parsedTime = explode('-', $time);
-        
-        $parsedTime[0] = date('H:i:s', strtotime($parsedTime[0]));
-        $parsedTime[1] = date('H:i:s', strtotime($parsedTime[1]));
-        
-        return $parsedTime;
-    }
-    
-    function parseLocation($location) {
-        $parsedLocation = array();
-        $parsedLocation = explode(' ', $location);
-        return $parsedLocation;
-    }
-    
-    function addEl($array, $el, $pos) {
-        return array_merge(array_slice($array, 0, $pos), array($el), array_slice($array, $pos));
-    }
- 
-    //run indefinitely
-    set_time_limit(0);
+	//remove whitespace
+	$field = trim($field);
+	return $field;
+}
 
-    //connect to db
-    include('database.php');
+function parseTime($time) {
+	if (strtolower($time) == 'tba') {
+		return array(NULL, NULL);
+	}
+	if (empty($time)) {
+		return array(NULL, NULL);
+	}
+	$parsedTime = array();
+	$parsedTime = explode('-', $time);
 
-    //select the file
-    $text = file_get_contents("courses.html");
+	$parsedTime[0] = date('H:i:s', strtotime($parsedTime[0]));
+	$parsedTime[1] = date('H:i:s', strtotime($parsedTime[1]));
 
-    //extract the right table
-    $startTable = stripos($text, '<table class="datadisplaytable"');
-    $text = substr($text, $startTable);
+	return $parsedTime;
+}
 
-    //start parsing (loop through all rows)
-    $insertId = null;
-    $start = stripos($text, '<tr');
-    while ($start !== false) {
-        $end = stripos($text, '</tr>', $start) + 5;
-        $offset = $end - $start;
+function parseLocation($location) {
+	if (strtolower($location) == 'tba') {
+		return array('TBA', NULL);
+	}
+	$parsedLocation = array();
+	$parsedLocation = explode(' ', $location);
+	if (count($parsedLocation) == 1) {
+		return array($parsedLocation[0], NULL);
+	}
+	return $parsedLocation;
+}
 
-        $tr = substr($text, $start, $offset);
+function addEl($array, $el, $pos) {
+	return array_merge(array_slice($array, 0, $pos), array($el), array_slice($array, $pos));
+}
 
-        //loop through all columns
-        $rowContent = array();
-        $colStart = stripos($tr, '<td');
-        while ($colStart !== false) {
-            $colEnd = stripos($tr, '</td>', $colStart) + 5;
-            $colOffset = $colEnd - $colStart;
+//run indefinitely
+set_time_limit(0);
 
-            $rowContent[] = cleanField(substr($tr, $colStart, $colOffset));
+//connect to db
+include('database.php');
 
-            $colStart = stripos($tr, '<td', $colEnd);
-        }
+//select the file
+$text = file_get_contents("courses.html");
 
-        if (empty($rowContent[1]) && !empty($rowContent)) {
-            array_unshift($rowContent, '');
-        }
-        if (count($rowContent) == 23) {
-            $rowContent = addEl($rowContent, '', 10);
-        }
-        
-//        print_r($rowContent);
+//extract the right table
+$startTable = stripos($text, '<table class="datadisplaytable"');
+$text = substr($text, $startTable);
 
-        if (!empty($rowContent)) {
+//start parsing (loop through all rows)
+$insertId = null;
+$start = stripos($text, '<tr');
+while ($start !== false) {
+	$end = stripos($text, '</tr>', $start) + 5;
+	$offset = $end - $start;
 
-            $crn = $rowContent[1];
-            $code = $rowContent[2];
-            $number = $rowContent[3];
-            $section = $rowContent[4];
-            $title = $rowContent[7];
-            $instructor = $rowContent[20];
+	$tr = substr($text, $start, $offset);
 
-            //fix time
-            $time = parseTime($rowContent[10]);
-            $startTime = $time[0];
-            $endTime = $time[1];
-            
-            $location = parseLocation($rowContent[22]);
-            $building = $location[0];
-            $room = $location[1];
-            
-            $days = $rowContent[9];
-            if (strtolower($days) == 'tba') {
-                $days = '';
-            }
-            $m = $t = $w = $r = $f = $sat = $sun = 0;
+	//loop through all columns
+	$rowContent = array();
+	$colStart = stripos($tr, '<td');
+	while ($colStart !== false) {
+		$colEnd = stripos($tr, '</td>', $colStart) + 5;
+		$colOffset = $colEnd - $colStart;
 
-            if (stripos($days, 'M') !== false) {
-                $m = 1;
-            }
-            if (stripos($days, 'T') !== false) {
-                $t = 1;
-            }
-            if (stripos($days, 'W') !== false) {
-                $w = 1;
-            }
-            if (stripos($days, 'R') !== false) {
-                $r = 1;
-            }
-            if (stripos($days, 'F') !== false) {
-                $f = 1;
-            }
-            if (stripos($days, 'S') !== false) {
-                $sat = 1;
-            }
+		$rowContent[] = cleanField(substr($tr, $colStart, $colOffset));
 
-            if (!empty($rowContent[1])) {
-                mysql_query("INSERT INTO courses_ (
+		$colStart = stripos($tr, '<td', $colEnd);
+	}
+
+	if (empty($rowContent[1]) && !empty($rowContent)) {
+		array_unshift($rowContent, '');
+	}
+	if (count($rowContent) == 23) {
+		$rowContent = addEl($rowContent, '', 10);
+	}
+
+	if (!empty($rowContent)) {
+
+		$crn = $rowContent[1];
+
+		echo $crn . '<br>';
+
+		$code = $rowContent[2];
+		$number = $rowContent[3];
+		$section = $rowContent[4];
+		$title = $rowContent[7];
+		$instructor = $rowContent[20];
+
+		//fix time
+		$time = parseTime($rowContent[10]);
+		$startTime = $time[0];
+		$endTime = $time[1];
+
+		//fix building
+		$location = parseLocation($rowContent[22]);
+		$building = $location[0];
+		$room = $location[1];
+
+		$days = $rowContent[9];
+		if (strtolower($days) == 'tba') {
+			$days = '';
+		}
+		$m = $t = $w = $r = $f = $sat = $sun = 0;
+
+		if (stripos($days, 'M') !== false) {
+			$m = 1;
+		}
+		if (stripos($days, 'T') !== false) {
+			$t = 1;
+		}
+		if (stripos($days, 'W') !== false) {
+			$w = 1;
+		}
+		if (stripos($days, 'R') !== false) {
+			$r = 1;
+		}
+		if (stripos($days, 'F') !== false) {
+			$f = 1;
+		}
+		if (stripos($days, 'S') !== false) {
+			$sat = 1;
+		}
+
+		if (!empty($rowContent[1])) {
+			mysql_query("INSERT INTO courses_ (
                     term, 
                     crn, 
                     subject, 
@@ -170,12 +181,12 @@
                     '$sun',
                     '$instructor'
                 )");
-                $insertId = mysql_insert_id();
-                if ($startTime == NULL) {
-                    mysql_query("UPDATE courses_ SET begin_time_1=NULL, end_time_1=NULL WHERE id='$insertId'");
-                }
-            } else {
-                mysql_query("UPDATE courses_ SET 
+			$insertId = mysql_insert_id();
+			if ($startTime == NULL) {
+				mysql_query("UPDATE courses_ SET begin_time_1=NULL, end_time_1=NULL WHERE id='$insertId'");
+			}
+		} else {
+			mysql_query("UPDATE courses_ SET 
                     begin_time_2='$startTime', 
                     end_time_2='$endTime', 
                     building_2='$building', 
@@ -189,12 +200,11 @@
                     sun_2='$sun', 
                     instructor_2='$instructor'
                     WHERE id='$insertId'");
-                if ($startTime == NULL) {
-                    mysql_query("UPDATE courses_ SET begin_time_2=NULL, end_time_2=NULL WHERE id='$insertId'");
-                }
-            }
-        }
-        $start = stripos($text, '<tr', $end);
-    }
-
+			if ($startTime == NULL) {
+				mysql_query("UPDATE courses_ SET begin_time_2=NULL, end_time_2=NULL WHERE id='$insertId'");
+			}
+		}
+	}
+	$start = stripos($text, '<tr', $end);
+}
 ?>
--- normalize.php
+++ normalize.php
@@ -1,92 +1,91 @@
 <?php
-	set_time_limit (0);
-	
-        function checkInstructor($name) {
-            $instructorResult = mysql_query("SELECT * FROM instructors WHERE name='$name'");
-            if (mysql_num_rows($instructorResult)) {
-                    $instructors = mysql_fetch_assoc($instructorResult);
-                    $instructor_id = $instructors['id'];
-            } else {
-                    mysql_query("INSERT INTO instructors (name) VALUES ('$name')");
-                    $instructor_id = mysql_insert_id();
-            }
-            return $instructor_id;
-        }
-        
-        function checkSubjects($code) {
-            $subjectResult = mysql_query("SELECT * FROM subjects WHERE code='$code'");
-            if (mysql_num_rows($subjectResult)) {
-                    $subjects = mysql_fetch_assoc($subjectResult);
-                    $subject_id = $subjects['id'];
-            } else {
-                    mysql_query("INSERT INTO subjects (code) VALUES ('$code')");
-                    $subject_id = mysql_insert_id();
-            }
-            return $subject_id;
-        }
-        
-        //connect to db
-	include('database.php');
-        
-	$result = mysql_query("SELECT * FROM courses_");
-	
-	while($row = mysql_fetch_array($result)) {
-		$code = $row['subject'];
-                $subject_id = checkSubjects($code);
-		$term = $row['term'];
-		$crn = $row['crn'];
-		$number = $row['course'];
-		$section = $row['section'];
-		$title = $row['title'];
-		$mon = $row['m_1'] || $row['m_2'];
-		$tue = $row['t_1'] || $row['t_2'];
-		$wed = $row['w_1'] || $row['w_2'];
-		$thu = $row['r_1'] || $row['r_2'];
-		$fri = $row['f_1'] || $row['f_2'];
-		$sat = $row['s_1'] || $row['s_2'];
-                
-                
-		mysql_query("INSERT INTO courses (term, crn, subject_id, number, section, title, m, t, w, r, f, s) 
+
+set_time_limit(0);
+
+function checkInstructor($name) {
+	$instructorResult = mysql_query("SELECT * FROM instructors WHERE name='$name'");
+	if ($instructorResult && mysql_num_rows($instructorResult)) {
+		$instructors = mysql_fetch_assoc($instructorResult);
+		$instructor_id = $instructors['id'];
+	} else {
+		mysql_query("INSERT INTO instructors (name) VALUES ('$name')");
+		$instructor_id = mysql_insert_id();
+	}
+	return $instructor_id;
+}
+
+function checkSubjects($code) {
+	$subjectResult = mysql_query("SELECT * FROM subjects WHERE code='$code'");
+	if (mysql_num_rows($subjectResult)) {
+		$subjects = mysql_fetch_assoc($subjectResult);
+		$subject_id = $subjects['id'];
+	} else {
+		mysql_query("INSERT INTO subjects (code) VALUES ('$code')");
+		$subject_id = mysql_insert_id();
+	}
+	return $subject_id;
+}
+
+//connect to db
+include('database.php');
+
+$result = mysql_query("SELECT * FROM courses_");
+
+while ($row = mysql_fetch_array($result)) {
+	$code = $row['subject'];
+	$subject_id = checkSubjects($code);
+	$term = $row['term'];
+	$crn = $row['crn'];
+	$number = $row['course'];
+	$section = $row['section'];
+	$title = $row['title'];
+	$mon = $row['m_1'] || $row['m_2'];
+	$tue = $row['t_1'] || $row['t_2'];
+	$wed = $row['w_1'] || $row['w_2'];
+	$thu = $row['r_1'] || $row['r_2'];
+	$fri = $row['f_1'] || $row['f_2'];
+	$sat = $row['sat_1'] || $row['sat_2'];
+
+
+	mysql_query("INSERT INTO courses (term, crn, subject_id, number, section, title, m, t, w, r, f, s) 
 				VALUES ('$term','$crn','$subject_id','$number','$section','$title', '$mon', '$tue', '$wed', '$thu', '$fri', '$sat')");
-		
-		$course_id = mysql_insert_id();
-		
-		$instructor_1 = $row['instructor_1'];
-		$instructor_2 = $row['instructor_2'];
-		
-		$instructor_id_1 = checkInstructor($instructor_1);
-		$instructor_id_2 = checkInstructor($instructor_2);
-		
-		$days = array($row['m_1'], $row['t_1'], $row['w_1'], $row['r_1'], $row['f_1'], $row['sat_1']);
-		
-		$startTime = $row['begin_time_1'];
-		$endTime = $row['end_time_1'];
-		$building = $row['building_1'];
-		$room = $row['room_1'];
-		
-		foreach($days as $key => $day) {
-			$dd = $key + 1;
-			if ($day == 1) {
-				mysql_query("INSERT INTO course_slots (start_time, end_time, building, room, instructor_id, day, course_id) 
+
+	$course_id = mysql_insert_id();
+
+	$instructor_1 = $row['instructor_1'];
+	$instructor_2 = $row['instructor_2'];
+
+	$instructor_id_1 = checkInstructor($instructor_1);
+	$instructor_id_2 = checkInstructor($instructor_2);
+
+	$days = array($row['m_1'], $row['t_1'], $row['w_1'], $row['r_1'], $row['f_1'], $row['sat_1']);
+
+	$startTime = $row['begin_time_1'];
+	$endTime = $row['end_time_1'];
+	$building = $row['building_1'];
+	$room = $row['room_1'];
+
+	foreach ($days as $key => $day) {
+		$dd = $key + 1;
+		if ($day == 1) {
+			mysql_query("INSERT INTO course_slots (start_time, end_time, building, room, instructor_id, day, course_id) 
 					VALUES ('$startTime','$endTime','$building','$room','$instructor_id_1','$dd','$course_id')");
-			}
-		}
-		
-		$days = array($row['m_2'], $row['t_2'], $row['w_2'], $row['r_2'], $row['f_2'], $row['sat_2']);
-		
-		$startTime = $row['begin_time_2'];
-		$endTime = $row['end_time_2'];
-		$building = $row['building_2'];
-		$room = $row['room_2'];
-		
-		foreach($days as $key => $day) {
-			$dd = $key + 1;
-			if ($day == 1) {
-				mysql_query("INSERT INTO course_slots (start_time, end_time, building, room, instructor_id, day, course_id) 
-					VALUES ('$startTime','$endTime','$building','$room','$instructor_id_2','$dd','$course_id')");
-			}
 		}
 	}
-	
 
+	$days = array($row['m_2'], $row['t_2'], $row['w_2'], $row['r_2'], $row['f_2'], $row['sat_2']);
+
+	$startTime = $row['begin_time_2'];
+	$endTime = $row['end_time_2'];
+	$building = $row['building_2'];
+	$room = $row['room_2'];
+
+	foreach ($days as $key => $day) {
+		$dd = $key + 1;
+		if ($day == 1) {
+			mysql_query("INSERT INTO course_slots (start_time, end_time, building, room, instructor_id, day, course_id) 
+					VALUES ('$startTime','$endTime','$building','$room','$instructor_id_2','$dd','$course_id')");
+		}
+	}
+}
 ?>
